cfb03179f919b536ea159f0fc84c674b2ad618ef
Thilini Meegama <thilini.meegama@volume.co.uk>
2020-07-07 14:10:20 +0530

    edit template

--- src/app/certification-exam/certification-exam.component.ts
+++ src/app/certification-exam/certification-exam.component.ts
@@ -33,4 +33,4 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+]; 
